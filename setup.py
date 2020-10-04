f50ede50e3a0164ef5d8480945796bbc1f6f7e7f
avra <stefan.avra@gmail.com>
2020-10-04 16:23:25 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fotoparadies-orders-cli',
+      version='0.1',
+      description='Unofficial command line interface for keeping track of dm Fotoparadies orders.',
+      url='https://github.com/StefanAvra/fotoparadies-orders-cli',
+      author='Stefan Avramescu',
+      author_email='stefan.avra@gmail.com',
+      license='',
+      packages=['fotoparadies-orders-cli'],
+      zip_safe=False)
