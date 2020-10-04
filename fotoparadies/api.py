23803e92324d01ed5fdf62a12abc1d2220ed475a
avra <stefan.avra@gmail.com>
2020-10-04 18:07:54 +0200

    added env var for api endpoint

--- fotoparadies/api.py
+++ fotoparadies/api.py
@@ -1,8 +1,12 @@
 import requests
+import os
 
 URL = ''
 with open('api_endpoint.txt') as f:
     URL = f.readline()
+
+if 'FOTOPARADIES_API_URL' in os.environ:
+    URL = os.environ['FOTOPARADIES_API_URL']
 
 def get_order_info(shop, order):
     p = {'config':1320, 'shop': shop, 'order': order}
