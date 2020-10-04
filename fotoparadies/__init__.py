ae29b74e8fc7ee587f0d6bed346d955560f73fa5
avra <stefan.avra@gmail.com>
2020-10-04 16:24:23 +0200

    added argparser to check order directly from cli

--- fotoparadies/__init__.py
+++ fotoparadies/__init__.py
@@ -1,13 +1,33 @@
 import curses
 from curses import panel
 from . import api
+import argparse
 
+
+parser = argparse.ArgumentParser(description='Check your photo orders from fotoparadies.')
+parser.add_argument('--shop', '-s', help='shop number, used for direct api call')
+parser.add_argument('--order', '-o', help='order number, used for direct api call')
+parser.add_argument('--json', help='return json of api call if using --shop and --order', action='store_true')
+
+args = parser.parse_args()
+
+if args:
+    if args.order and args.shop:
+        order_info = api.get_order_info(args.shop, args.order)
+        if args.json:
+            print(order_info)
+        else:
+            print(f'{order_info["summaryDate"]}: {order_info["summaryStateCode"]}')
+            print(f'{order_info["summaryStateText"]}')
+            print(f'Total of {order_info["summaryPriceText"]}'.replace(u'\xa0', u' '))
+        exit()
 
 class Menu(object):
     def __init__(self, items, stdscr):
-        self.window = stdscr.subwin(0,0)
+        self.window = stdscr.subwin(3,3)
         self.window.keypad(1)
         self.panel = panel.new_panel(self.window)
+        self.panel.hide()
         panel.update_panels()
 
         self.items = items
@@ -22,6 +42,7 @@ class Menu(object):
 
     def display(self):
         self.panel.top()
+        self.panel.show()
         self.window.clear()
 
         while True:
@@ -33,8 +54,8 @@ class Menu(object):
                 else:
                     mode = curses.A_NORMAL
 
-                msg = f'{index}  {item[0]}'
-                self.window.addstr(1 + index, 1, msg, mode)
+                msg = f'{index + 1}  {item[0]}'
+                self.window.addstr(index + 1, 1, msg, mode)
 
             key = self.window.getch()
 
@@ -52,27 +73,31 @@ class Menu(object):
                 break 
     
     
-def open_orders():
-    pass
 
 def new_order():
     pass
 
+
 def main(stdscr):
+
     stdscr.clear()
     curses.curs_set(0)
+    stdscr.addstr(2, 4, 'fotoparadies orders cli')
 
-    menus = {
-    'home': [
-        ('orders', open_orders),
-        ('check/add order', new_order),
+    orders_menu_entries = [
+        ('test', 0),
+        ('back', 0)
+    ]
+    orders_menu = Menu(orders_menu_entries, stdscr)
+
+    home_menu_entries = [
+        ('see orders', orders_menu.display),
+        ('check/add new order', new_order),
         ('exit',0)
-        ],
-    }
+    ]
+    home_menu = Menu(home_menu_entries, stdscr)
 
-    stdscr.addstr(2, 2, 'Welcome!')
 
-    home_menu = Menu(menus['home'], stdscr)
     home_menu.display() 
 
     stdscr.refresh()
